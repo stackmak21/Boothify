bd745aad6cd38ef7c9acc95b77b725a92c519050
stackmak21 <stackmak21@gmail.com>
2024-05-21 22:39:07 +0300

    first commit

---
+++ Boothify/BoothifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoothifyApp.swift
+//  Boothify
+//
+//  Created by Paris Makris on 21/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BoothifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
